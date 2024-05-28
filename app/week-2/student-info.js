c333449159129f2558517fb3a06c5d5b47cb9b81
mandyl94910 <132793189+mandyl94910@users.noreply.github.com>
2024-05-28 10:31:14 -0600

    assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+      <div>
+        <h1>Mengdie Liu</h1>
+        <ul>
+        <li><Link href= "https://github.com/mandyl94910/cprg306-assignments"> https://github.com/mandyl94910/cprg306-assignments</Link></li>
+      </ul>
+
+      </div>
+    );
+  }
